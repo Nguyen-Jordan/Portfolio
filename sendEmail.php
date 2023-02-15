6d640fba834d7ff6210f5b1da5ee97edb1787678
jordan-nguyen <dizorder3@gmail.com>
2023-02-15 22:07:34 +0100

    modify senEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -2,15 +2,15 @@
 
 $destination = "nguyenjordan89@gmail.com";
 
-$firstname = $_POST['prénom'];
-$lastname = $_POST['nom'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
 $number = $_POST['number'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $header = "Envoi depuis le formulaire de contact";
-$messageComplete = "\n Prénom :" . $firstname . "\n" . "Email :" . $email . "\n" . "Telephone :" . $number . "\n" .
+$messageComplete = "\n" . "Prénom :" . $firstname . "\n" . "Nom :" . $lastname . "\n" . "Email :" . $email . "\n" . "Telephone :" . $number . "\n" .
   "Subject :" . $subject . "\n" . "Message" . $message;
 mail($destination, $subject, $messageComplete, $header);
 header('Location: index.html');
